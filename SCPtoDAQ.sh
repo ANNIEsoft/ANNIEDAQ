03ec3cbbd8cf4d7717a89105eef58191d4c418af
Exp/P1063/Annie <annie@annie-daq01.fnal.gov>
2016-05-15 08:03:44 -0500

    before tirgger update

--- SCPtoDAQ.sh
+++ SCPtoDAQ.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
 scp  192.168.163.36:/home/annie/ANNIEDAQ/* /home/annie/VME/ANNIEDAQ/
-scp  192.168.163.36:/home/annie/ANNIEDAQ/configfiles/* /home/annie/VME/ANNIEDAQ/configfiles
-scp  192.168.163.36:/home/annie/ANNIEDAQ/DataModel/* /home/annie/VME/ANNIEDAQ/Datamodel
-scp  192.168.163.36:/home/annie/ANNIEDAQ/src/* /home/annie/VME/ANNIEDAQ/src
-scp  -r 192.168.163.36:/home/annie/ANNIEDAQ/UserTools/* /home/annie/VME/ANNIEDAQ/UserTools
+scp  192.168.163.36:/home/annie/ANNIEDAQ/configfiles/* /home/annie/VME/ANNIEDAQ/configfiles/
+scp  192.168.163.36:/home/annie/ANNIEDAQ/DataModel/* /home/annie/VME/ANNIEDAQ/DataModel/
+scp  192.168.163.36:/home/annie/ANNIEDAQ/src/* /home/annie/VME/ANNIEDAQ/src/
+scp  -r 192.168.163.36:/home/annie/ANNIEDAQ/UserTools/* /home/annie/VME/ANNIEDAQ/UserTools/
 scp -r 192.168.163.36:/home/annie/ANNIEDAQ/ToolDAQ/ToolDAQFramework /home/annie/VME/ANNIEDAQ/ToolDAQ/
